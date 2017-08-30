c7d7f12150abd7b262053a6b07b4dc75987755f1
Barbosa De Andrade <vsde@gft.com>
2017-08-30 16:41:35 -0300

    gulp unusual improvement

--- Application/View/gulpfile.js
+++ Application/View/gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     uglifycss = require('gulp-uglifycss'),
     concat = require('gulp-concat'),
     inject = require('gulp-inject'),
-    htmlmin = require('gulp-htmlmin');
+    htmlmin = require('gulp-htmlmin'),
+    del = require('del');
 
 var dev = 'build/',
     dev_asciiworld = 'build/asciiworld',
@@ -29,67 +30,70 @@ gulp.task('default', function() {
     console.log('\n');
 });
 
-gulp.task('build-dev', ['build-unusual-dev']);
-gulp.task('build-prod', ['prod-js', 'prod-css', 'prod-images', 'dev-fonts', 'prod-index']);
 
 gulp.task('unusual-dev', ['dev-js', 'dev-css', 'dev-images', 'dev-fonts', 'dev-index']);
-gulp.task('build-unusual-dev', ['dev-min-js', 'dev-min-css', 'dev-images', 'dev-fonts', 'dev-min-index']);
+gulp.task('unusual-dev-min', ['dev-min-js', 'dev-min-css', 'dev-images', 'dev-fonts', 'dev-min-index']);
+
+gulp.task('dev-injection', function() {
+    gulp.src(dev+'index.html')
+        .pipe(
+            inject(
+                gulp.src(['./build/js/*.js', './build/css/*.css'], { read: false }),
+                {relative: true})
+            )
+        .pipe(gulp.dest(dev));
+});
+
 
 gulp.task('dev-min-js', function() {
-   gulp.src('js/*.js')
-      .pipe(uglify())
-      .pipe(concat('app.js'))
-      .pipe(gulp.dest(dev+'js/'));
+    del([dev+'js/*']);
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(dev+'js/'));
 });
 
 gulp.task('dev-min-css', function() {
-   gulp.src('css/*.css')
-      .pipe(uglifycss())
-      .pipe(concat('styles.css'))
-      .pipe(gulp.dest(dev+'css/'));
+    del([dev+'css/*']);
+    gulp.src('css/*.css')
+        .pipe(uglifycss())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest(dev+'css/'));
 });
 
 gulp.task('dev-js', function() {
-   gulp.src('js/*.js')
-      .pipe(gulp.dest(dev+'js/'));
+    del([dev+'js/*']);
+    gulp.src('js/*.js')
+        .pipe(gulp.dest(dev+'js/'));
 });
 
 gulp.task('dev-css', function() {
-   gulp.src('css/*.css')
-      .pipe(gulp.dest(dev+'css/'));
+    del([dev+'css/*']);
+    gulp.src('css/*.css')
+        .pipe(gulp.dest(dev+'css/'));
 });
 
 gulp.task('dev-images', function() {
-    gulp.src('img/**/*.*')
+    del([dev+'img/*']);
+    gulp.src('img/**/*')
         .pipe(gulp.dest(dev+'img/'));
 });
 
 gulp.task('dev-fonts', function() {
-    gulp.src('font/**/*.*')
+    del([dev+'font/*']);
+    gulp.src('font/**/*')
         .pipe(gulp.dest(dev+'font/'));
 });
 
 gulp.task('dev-index', function () {
+    del([dev+'index.html']);
     gulp.src('index.html')
-        .pipe(gulp.dest(dev));
-    gulp.src(dev+'index.html')
-        .pipe(
-            inject(
-                gulp.src(['build/js/*.js', 'build/css/*.css'], { read: false }),
-                {relative: true})
-            )
         .pipe(gulp.dest(dev));
 });
 
 gulp.task('dev-min-index', function () {
+    del([dev+'index.html']);
     gulp.src('index.html')
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest(dev));
-    gulp.src(dev+'index.html')
-        .pipe(
-            inject(
-                gulp.src(['build/js/*.js', 'build/css/*.css'], { read: false }),
-                {relative: true})
-            )
         .pipe(gulp.dest(dev));
 });
